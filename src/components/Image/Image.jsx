f0658e965d1d4fdf1846576a4e1f2e0be020b4c7
Grant <gmontgomery1994@gmail.com>
2019-12-25 21:45:09 -0800

    card flipping on click.

--- src/components/Image/Image.jsx
+++ src/components/Image/Image.jsx
@@ -4,21 +4,35 @@ import "./Image.css";
 class Image extends Component {
   constructor(props) {
     super(props);
+    this.state = {
+      animate: ""
+    };
   }
+
+  handleFlip = event => {
+    event.preventDefault();
+    const { animate } = this.state;
+    animate === ""
+      ? this.setState({ animate: "flip" })
+      : this.setState({ animate: "" });
+  };
 
   render() {
     const { url, alt } = this.props;
     return (
-      <div className="card-wrapper">
+      <div
+        className={`card-wrapper ${this.state.animate}`}
+        onClick={this.handleFlip}
+      >
         <div className="card-inner">
           <div className="flip-front">
             <img src={url} alt={alt} />
           </div>
-        </div>
-        <div className="flip-back">
-          <h1>Cool Card</h1>
-          <p>Bruh</p>
-          <p>uhhh</p>
+          <div className="flip-back">
+            <h1>Cool Card</h1>
+            <p>Bruh</p>
+            <p>uhhh</p>
+          </div>
         </div>
       </div>
     );
